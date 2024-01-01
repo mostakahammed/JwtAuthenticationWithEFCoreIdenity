91c183055fcbb09da187e6aa3f67ce95844dfda6
Mostak Ahammed <mostakahammedofficial@gmail.com>
2024-01-02 00:28:35 +0600

    Repository

---
+++ JWT.API/Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace JWT.API.Domain.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        T GetById(int id);
+        T Get(Expression<Func<T>> expression);
+    }
+}
